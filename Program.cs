fc890274c6ce123335304a6601c916bf3a3d2667
Gabriel Cardoso <gabrielfaria00@hotmail.com>
2024-04-01 12:23:29 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace InversoQuadrado
+{
+    class Program
+    {
+    static void Main(string[] args)
+
+    {
+        //Array
+        int[] vet = new int [5];
+        int i = 0;
+
+        //Números que serão fornecidos pelo usuário
+        for (i = 0; i < 5; i++)
+        {
+            Console.Write($"Valor {i + 1} : ");
+            vet[i] = int.Parse(Console.ReadLine());
+        }
+
+        //Inverso dos números
+        Console.Write("Inverso: ");
+        for (i = 4; i >= 0; i--)
+        {
+            Console.Write(vet[i] + " ");
+        }
+
+        Console.WriteLine(" ");
+
+        //Quadrado dos números
+        Console.Write("Quadrado: ");
+        for (i = 0; i < 5; i++)
+        {
+            Console.Write(vet[i] * vet[i] + " ");
+        }
+        
+        Console.WriteLine(" ");
+
+        //Números maiores que 3
+        Console.Write("Números maiores que 3: ");
+        for (i = 0; i < 5; i++)
+        {
+            if (vet[i] > 3)
+            {
+                Console.Write(vet[i] + " ");
+            }
+        }
+
+        Console.WriteLine(" ");
+
+        //Indices de números pares10
+        Console.Write("Números pares: ");
+        for (i = 0; i < 5; i++)
+        {
+            if (vet[i] % 2 == 0)
+            {
+                Console.Write(vet[i] + " ");
+            }
+        }
+        
+        Console.WriteLine(" ");
+    }
+
+    }
+}
